3a1cf0adbd88bf2d79eff8aa9045df36927a6d2d
DESKTOP-MGU93L1\anude <anudeepsuraneni@gmail.com>
2025-06-03 17:58:17 +0530

    Revert "fixed build error"
    This reverts commit f405edd424384e8c4b70b33684024bca70d8926a.

--- src/assets/configs/footerConfig.js
+++ src/assets/configs/footerConfig.js
@@ -4,13 +4,13 @@ import {SiKaggle, SiTableau} from "react-icons/si";
 
 const footerConfig = {
     icons: [
-        {
-            id: "GitHub",
-            url: "https://github.com/anudeepsuraneni",
-            className: "social-icon",
-            target: "_blank",
-            icon: <AiFillGithub size={50}/>
-        },
+        // {
+        //     id: "GitHub",
+        //     url: "https://github.com/anudeepsuraneni",
+        //     className: "social-icon",
+        //     target: "_blank",
+        //     icon: <AiFillGithub size={50}/>
+        // },
         {
             id: "Kaggle",
             url: "https://www.kaggle.com/anudeepsuraneni",
@@ -18,13 +18,13 @@ const footerConfig = {
             target: "_blank",
             icon: <SiKaggle size={50}/>
         },
-        {
-            id: "Tableau",
-            url: "https://public.tableau.com/app/profile/anudeep7780/vizzes",
-            className: "social-icon",
-            target: "_blank",
-            icon: <SiTableau size={50}/>
-        },
+        // {
+        //     id: "Tableau",
+        //     url: "https://public.tableau.com/app/profile/anudeep7780/vizzes",
+        //     className: "social-icon",
+        //     target: "_blank",
+        //     icon: <SiTableau size={50}/>
+        // },
         {
             id: "LinkedIn",
             url: "https://www.linkedin.com/in/anudeep-suraneni/",
