3e13fbb80d00c3825951d42f1ecef07b57eb0cae
DESKTOP-MGU93L1\anude <anudeepsuraneni@gmail.com>
2025-06-04 16:48:30 +0530

    added cyclistic tableau and presentation links

--- src/assets/configs/projectConfig.js
+++ src/assets/configs/projectConfig.js
@@ -1,4 +1,5 @@
-import {FaKaggle} from "react-icons/fa";
+import {FaKaggle, FaRegEye} from "react-icons/fa";
+import {SiTableau} from "react-icons/si";
 import cyclisticLogo from "../projects/cyclistic/logo.png"
 
 const projectConfig = [
@@ -10,6 +11,16 @@ const projectConfig = [
                 name: "Kaggle Notebook",
                 url: "https://www.kaggle.com/code/anudeepsuraneni/how-does-a-bike-share-navigate-speedy-success",
                 icon: <FaKaggle />
+            },
+            {
+                name: "Tableau Dashboard",
+                url: "https://public.tableau.com/views/HowDoesaBike-ShareNavigateSpeedySuccess_17490155704140/RidershipbetweenCasualRidersandAnnualMembers?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+                icon: <SiTableau />
+            },
+            {
+                name: "Executive Summary",
+                url: "https://docs.google.com/presentation/d/1Fe4hTNBd_gMWMHCTpdQ2ozi8gHiuvlYfJgCWwgiELLs/edit?usp=sharing",
+                icon: <FaRegEye />
             }
         ],
         image: cyclisticLogo,
