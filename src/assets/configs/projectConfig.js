c1a8222cc9187439f6993240899fa51df5a0e1aa
DESKTOP-MGU93L1\anude <anudeepsuraneni@gmail.com>
2025-06-11 22:38:09 +0530

    updated bellabeat logo

--- src/assets/configs/projectConfig.js
+++ src/assets/configs/projectConfig.js
@@ -6,25 +6,6 @@ import bellabeatLogo from "../../projects/bellabeat/logo.png";
 const projectConfig = [
     {
         id: "project-2",
-        title: "How Can a Wellness Technology Company Play It Smart?",
-        links: [
-            {
-                name: "Kaggle Notebook",
-                url: "https://www.kaggle.com/code/anudeepsuraneni/how-can-a-wellness-tech-company-play-it-smart",
-                icon: <FaKaggle />
-            },
-            {
-                name: "Executive Summary",
-                url: "https://docs.google.com/presentation/d/1RmR7E-n8LG3ChkKzMdp_hvu58NEcJupVKOYmFJNkl4g/edit?usp=sharing",
-                icon: <FaRegEye />
-            }
-        ],
-        image: bellabeatLogo,
-        description: "A data-driven exploration of Fitbit smart device usage to uncover usage trends and inform strategic marketing recommendations for a wellness tech company.",
-        target: "_blank"
-    },
-    {
-        id: "project-1",
         title: "How does a bike-share navigate speedy success?",
         links: [
             {
@@ -46,7 +27,26 @@ const projectConfig = [
         image: cyclisticLogo,
         description: "A comprehensive analysis of bike-share data to understand user behavior and improve business strategies.",
         target: "_blank"
-    }
+    },
+    {
+        id: "project-1",
+        title: "How Can a Wellness Technology Company Play It Smart?",
+        links: [
+            {
+                name: "Kaggle Notebook",
+                url: "https://www.kaggle.com/code/anudeepsuraneni/how-can-a-wellness-tech-company-play-it-smart",
+                icon: <FaKaggle />
+            },
+            {
+                name: "Executive Summary",
+                url: "https://docs.google.com/presentation/d/1RmR7E-n8LG3ChkKzMdp_hvu58NEcJupVKOYmFJNkl4g/edit?usp=sharing",
+                icon: <FaRegEye />
+            }
+        ],
+        image: bellabeatLogo,
+        description: "A data-driven exploration of smart device usage data to uncover usage trends and inform strategic marketing recommendations.",
+        target: "_blank"
+    },
 ]
 
 export default projectConfig
